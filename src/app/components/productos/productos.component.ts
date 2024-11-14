c5fae1669cf9087654056a41036fb6caa3ce5cc4
kiri-1 <belenjesuslp@gmail.com>
2024-11-14 19:01:26 -0300

    trae productos

--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AerolabService } from 'src/app/services/aerolab.service';
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
   styleUrls: ['./productos.component.css']
 })
-export class ProductosComponent {
+export class ProductosComponent implements OnInit {
+  product: any;
 
+  constructor(
+    private route: ActivatedRoute,
+    private aerolabService: AerolabService
+  ) {}
+
+  async ngOnInit() {
+    const productId = this.route.snapshot.paramMap.get('id')!;
+    this.product = await this.aerolabService.getProductById(productId);
+  }
 }
